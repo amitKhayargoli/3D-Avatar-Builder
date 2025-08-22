79ba002697ef1d383ffe8d2a2080a551ec7742a2
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-22 23:52:39 +0545

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <>
+    <Canvas
+    camera={{poisiion: [3,3,3],}}>
+      <color attach="background" args={['#202020']} />
+      <OrbitControls/>
+      <mesh>
+        <boxGeometry args={[0.5, 0.5, 0.5]} />
+        <meshNormalMaterial/>
+      </mesh>
+    </Canvas>
+    </>
+)
 }
 
 export default App
